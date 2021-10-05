7ee7aba8d35b3f8449ea508975a98a0fd17c287f
sarjid777 <sarjid777@gmail.com>
2021-10-05 10:38:24 +0600

    about API calls

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,24 @@ import contact from "../views/Contact.vue";
 //import nprogress
 import NProgress from "nprogress";
 
+// Sometimes calling an API would necessitate without any change in URL. It means Vue route guards will not work here, and no more calls to beforeEnter or beforeEach will be made. In this case, you will need to deal with your HTTP client. If you are using Axios, it allows you to intercept your requests and responses by using interceptors and add some functionalities, like starting and stopping a progress bar just like this:
+
+// const apiClient = axios.create({ ... })
+
+// // Called on request
+
+// apiClient.interceptors.request.use(config => { 
+//   NProgress.start()
+//   return config
+// })
+
+// // Called on response
+
+// apiClient.interceptors.response.use(response => { 
+//   NProgress.done()
+//   return response
+// })
+
 const routes = [
   {
     path: "/",
